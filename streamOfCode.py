614abfae517f1a511f5c2c00a5fbfce787f6e9e0
MMORONTO <t2wycemo@gmail.com>
2022-09-07 12:06:50 -0500

    update peutingar Stream-of-code page

--- streamOfCode.py
+++ streamOfCode.py
@@ -30,6 +30,7 @@ with st.container():
          by enabling the monitoring of certain infrastructure at scale with minimum 
          deployment of ground teams. This way troubled spots are targeted and 
          prioritized thus saving man hours and budget.''')
+
 with st.container():
    image_col, text_col = st.columns((1,2))
    with image_col:
@@ -39,9 +40,39 @@ with st.container():
       st.write('''
          "If you want to build a wealthy economy, you must first start by building roads". 
          I don't recall exactly when I first heard this quote. It is an old Chinese saying, but then again, I can't be so sure. 
-         This quote resonates when you consider fast-growing nations in the last century. 
+         This quote resonates when you consider the nations that have grown the fastest in the last century. 
          Much of their accomplishments were possible due to effective transportation infrastructure. 
          Road and rail networks are essential for opening up a country's interior. 
          This is even more so if such regions were hitherto, inaccessible. 
          Roads foster cohesion across regions thereby facilitating trade and commerce amongst neighbors. 
          Roads are accelerants for developing manufacturing and industrial capacity in a region.''')
+
+with st.container():
+   image_col, text_col = st.columns((1,2))
+   with image_col:
+      st.image("https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1575660327927x644858521477206700%2Fhttp_%25252F%25252Fcdn.cnn.com%25252Fcnnnext%25252Fdam%25252Fassets%25252F180518162730-edward-burtynsky-lagos.jpg?w=2048&h=941&auto=compress&dpr=1&fit=max")
+   with text_col:
+      st.markdown("**Trans African Transportation Networks**")
+      st.write('''
+         "I have seen and heard quite a number of media sources talk about trans African 
+         highways and railway lines. The voice in my head cannot help but mutter that these 
+         are most likely myth than reality.  These types of projects are far from trivial. 
+         They require tight coordination across multiple stake holders, and this is even 
+         moreso when multinational interests are at stake.
+
+         Integrating the African continent with a trans-continental highway or railway network would 
+         require a colossal effort so immense only a top-down  command/control style of administration 
+         and financing is conceivable for such massive projects to be achieved. It would require a body 
+         with continent wide clout & reach like the AU on steroids to fund it(or at least to administer 
+         via a common fund) and oversee every phase of the projects while superseding national 
+         jurisdictional rights or interests during the design/build phase of the projects. In the 
+         improbable likelihood that such a project is pulled off, it would be the largest and most 
+         ambitious project ever pulled off by mankind owing to the sheer size of the African continent.
+
+         It is unfortunate that such a project though beneficial to humankind is most unlikely to succeed 
+         owing to the fractured nature of the constituent nations on the continent, their disparate interests 
+         and the vastness in the degree of unalignment that would exist among the pertinent stakeholders.  
+         Financing would most likely remain the largest boulder to overcome as the complexities of an 
+         emergent multipolar geopolitical world start to play out over the short and long terms. It is 
+         conceivable that financiers from opposing geopolitical poles find reason to acknowledge common 
+         interests in funding Africa's bedrock infrastructure. A long shot? Maybe, only time will tell.''')
