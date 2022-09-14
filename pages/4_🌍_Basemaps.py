fd1e2871ee0ba24a5a8d87b0afc4f2d5cd17ff8b
MMORONTO <t2wycemo@gmail.com>
2022-09-14 14:26:41 -0500

    lay out basemaps page

--- pages/4_🌍_Basemaps.py
+++ pages/4_🌍_Basemaps.py
@@ -7,24 +7,32 @@ st.header("Lagos")
 st.subheader("View road report analyses")
 st.write("The state of transportation network assets.")
 
-map_data = pd.DataFrame(
-    np.random.randn(100, 2) / [50, 50] + [6.46, 3.406],
-    columns=['lat', 'lon'])
+col1, col2 = st.columns([1, 2])
 
-st.map(map_data)
+with col2:
+    map_data = pd.DataFrame(
+        np.random.randn(100, 2) / [50, 50] + [6.46, 3.406],
+        columns=['lat', 'lon'])
+
+    st.map(map_data)
+
+with col1:
+    st.image('''./distressedRoad.png''')
+
+with col1:
+    if st.checkbox('Show dataframe'):
+        chart_data = pd.DataFrame(
+           np.random.randn(20, 3),
+           columns=['a', 'b', 'c'])
+
+        chart_data
+
+    df = pd.DataFrame({
+        'first column': [1, 2, 3, 4],
+        'second column': [10, 20, 30, 40]
+        })
 
 
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
-
-    chart_data
-
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    })
 
 
 
